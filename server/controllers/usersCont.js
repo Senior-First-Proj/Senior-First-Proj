871501bea43f356e616d0df413973a0aa7536eb0
malekaloui1 <144437590+malekaloui1@users.noreply.github.com>
2023-12-15 11:15:20 +0100

    Melek (#18)
    * 15 dec
    
    * melek controller

--- server/controllers/usersCont.js
+++ server/controllers/usersCont.js
@@ -13,17 +13,17 @@ const getUsers=(req,res)=>{
 
 // getOne (to get one user by userName)
 const getOne=(req,res)=>{
-    readOne(req.params.name,(err,result)=>{result?res.json(result):console.log(err)})
+    readOne(req.params.id,(err,result)=>{result?res.json(result):console.log(err)})
 }
 
 // updateUser (to update a user by userName)
 const updateUser=(req,res)=>{
-    upUser(req.params.name,req.body.name.req.body.lastName,req.body.picture,(err,result)=>{result?res.json(result):console.log(err)})
+    upUser(req.params.id,req.body,(err,result)=>{result?res.json(result):console.log(err)})
 }
 
 //deleteUser (to delete a user by userName)
 const deleteUser=(req,res)=>{
-    removeUser(req.params.name,(err,result)=>{result?res.json(result):console.log(err)})
+    removeUser(req.params.id,(err,result)=>{result?res.json(result):console.log(err)})
 }
 
 //Don't forget to export
