41382990b6c4f7d483d2b3d16843c9cd7c3a1306
malekaloui1 <malekaloui888@gmail.com>
2023-12-15 13:05:19 +0100

    comment updated

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -4,7 +4,7 @@ const {readComByPost,createCom,upCom,removeCom} = require('../models/commentsMod
 
 //getCom by post id
 const getCom=(req,res)=>{
-    readComByPost(req.params.id,(err,result)=>{result?res.json(result):console.log(err)})
+    readComByPost(req.params.idposts,(err,result)=>{result?res.json(result):console.log(err)})
 }
 
 //addCom
@@ -15,12 +15,12 @@ const addCom=(req,res)=>{
 
 //updateCom
 const updateCom=(req,res)=>{
-    upCom(req.parmas.id,req.body,(err,result)=>{result?res.json(result):console.log(err)})
+    upCom(req.params.idcomments,req.body,(err,result)=>{result?res.json(result):console.log(err)})
 }
 
 //deleteCom
 const deleteCom=(req,res)=>{
-    removeCom(req.params.id,(err,result)=>{result?res.json(result):console.log(err)})
+    removeCom(req.params.idcomments,(err,result)=>{result?res.json(result):console.log(err)})
 }
 
 
