262c79c232b7b5693a313a43dae8de7d07fab265
azizchannoufi <channoufi2003@gmail.com>
2023-12-15 13:17:45 +0100

    aziz

--- sign_up-login/server/routes/bankRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const bankController = require("../controllers/bankController");
-
-router.get("/getAll", bankController.getUser);
-router.post("/adduser",bankController.addUser)
-
-module.exports = router;
