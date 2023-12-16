84f2ea06e7845b997dbf497e7e47041bae299a35
Aziz Channoufi <42783905+azizchannoufi@users.noreply.github.com>
2023-12-16 11:24:56 +0100

    Aziz test fixing (#36)
    * aziz first push
    
    * aziz
    
    * azizcomitfront
    
    * packageadd
    
    * lastadd
    
    * lastpush
    
    * lastadd
    
    * aziz test fixing
    
    ---------
    
    Co-authored-by: Farahkharbech <134403825+Farahkharbech@users.noreply.github.com>

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import Formul from './components/formulaire.jsx'
+import Formulaire from './components/Formulaire.jsx'
 import Login from './components/Login.jsx'
 import axios from 'axios'
 import Alert from '@mui/material/Alert';
@@ -15,6 +15,7 @@ const App = () => {
   const [show,setshow]=useState(true)
   console.log(users);
 //back:
+ 
  // *********************************Users interaction ***********************//
 
  //get All users // 
@@ -39,100 +40,11 @@ const adduser=(user)=>{
   .then(()=>{console.log("user added");
     setshow(false)
   })
-  .catch((err)=>{console.log(err)})}
-
-// delete user // 
-
-const deleteUser = (idUser) => {
-  axios.delete(`http://localhost:3000/api/users/deleteUser/:${idUser}`)
-  .then(()=>{ console.log("user deleted")})
-  .catch((err)=>{ console.log(err)})
+  .catch((err)=>{
+    console.log(err);
+  })
 }
 
-
-// update User // 
-
-const updateUser = (idUser,newdat) => {
-  axios.put(`http://localhost:3000/api/users/updateUser/:${idUser}`,newdat)
-  .then(()=>{ console.log("user updated")})
-  .catch((err)=>{ console.log(err)})
-}
-
- // *********************************Posts interaction ***********************//
-//get All posts // 
-
-useEffect(()=>{
-  axios.get("http://localhost:3000/api/posts/getPosts/allposts")
-  .then((res)=>{console.log("all posts")})
-  .catch((err)=>{console.log(err)})
-},[])
-
-// get posts of one selected user // 
-const getUserPosts = (idUser) => {
-  axios.get(`http://localhost:3000/api/posts/getPostByUser/:${idUser}`)
-  .then(()=>{ console.log("posts of user getted")})
-  .catch((err)=>{ console.log(err)})
-}
-
-// get Posts by category // 
-
-const getPostsCat = (idCat) => {
-  axios.get(`http://localhost:3000/api/posts/getPostByCat/:${idCat}`)
-  .then(()=>{ console.log("posts by category getted")})
-  .catch((err)=>{ console.log(err)})
-}
-
-// add Post //
-const addPost=(post)=>{
-  axios.post("http://localhost:3000/api/posts/addPost",post)
-  .then(()=>{console.log("Post added")})
-  .catch((err)=>{console.log(err)})}
-
-// delete Post // 
-const deletePost=(idPost)=>{
-  axios.delete(`http://localhost:3000/api/posts/deletePost/:${idPost}`)
-  .then(()=>{console.log("Post deleted")})
-  .catch((err)=>{console.log(err)})}
-
-// update Post // 
-
-const updatePost=(idPost,newData)=>{
-  axios.put(`http://localhost:3000/api/posts/updatePost/:${idPost}`,newData)
-  .then(()=>{console.log("Post updated")})
-  .catch((err)=>{console.log(err)})}
-
- // *********************************Comments interaction ***********************//
-// Get comments by post // 
- const getComByPost = (idPost) => {
-  axios.get(`http://localhost:3000/api/comments/allcomments/:${idPost}`)
-  .then(()=>{ console.log("Comments by Post getted")})
-  .catch((err)=>{ console.log(err)})
-}
-
-// Add comment 
-const addComment = (newCom) => {
-  axios.post(`http://localhost:3000/api/comments/addComment`,newCom)
-  .then(()=>{ console.log("Comments added")})
-  .catch((err)=>{ console.log(err)})
-}
-// Delete comment //
-const deleteComment = (idCom) => {
-  axios.delete(`http://localhost:3000/api/comments/deleteCom/:${idCom}`)
-  .then(()=>{ console.log("Comments deleted")})
-  .catch((err)=>{ console.log(err)})
-}
-
-// Update Comment //
-
-const updateComment = (idCom,newD) => {
-  axios.put(`http://localhost:3000/api/comments/updateCom/:${idCom}`,newD)
-  .then(()=>{ console.log("Comments updated")})
-  .catch((err)=>{ console.log(err)})
-}
-
- // *********************************Categories interaction ***********************//
-
-// get All categories // 
 useEffect(()=>{
   axios.get("http://localhost:3000/api/categories/allCats")
   .then((res)=>{console.log("all Categories")})
